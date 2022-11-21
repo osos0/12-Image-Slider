9980c4e1b7fe10ef42f14900a29b0ffe32d81731
unknown <osama_magdy0@yahoo.com>
2022-11-21 22:31:30 +0200

    js codes from my home

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const contanerEl = document.querySelector(".contaner");
 const btnL = document.querySelector(".fa-angles-left");
 const btnR = document.querySelector(".fa-angles-right");
+const img1 = document.getElementsByTagName("img")[0];
+console.log(img1);
 
 // btnL.addEventListener("click", () => {
-//   // contanerEl
+//   //   console.log(`l`);
+//   //   img1.style.transform = "translateX(-500px)";
 // });
+
+btnR.addEventListener("click", () => {
+  img1.classList.add("active") + 500;
+  console.log(33);
+  console.log(img1);
+});
