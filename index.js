1628662eb1c1517a9f328a8d606137dfcf1aef6b
osos0 <osama_magdy0@yahoo.com>
2022-11-21 15:42:17 +0200

    the second upload from work

---
+++ index.js
@@ -0,0 +1,7 @@
+const contanerEl = document.querySelector(".contaner");
+const btnL = document.querySelector(".fa-angles-left");
+const btnR = document.querySelector(".fa-angles-right");
+
+// btnL.addEventListener("click", () => {
+//   // contanerEl
+// });
