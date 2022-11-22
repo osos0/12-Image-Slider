a88e976c6e70ff4c9418fe1716ba33f9cda8799c
unknown <osama_magdy0@yahoo.com>
2022-11-22 06:26:23 +0200

    js not finish yet

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 const contanerEl = document.querySelector(".contaner");
 const btnL = document.querySelector(".fa-angles-left");
 const btnR = document.querySelector(".fa-angles-right");
-const img1 = document.getElementsByTagName("img")[0];
-console.log(img1);
+const imgEl = document.getElementsByTagName("img");
+// console.log(img);
 
 // btnL.addEventListener("click", () => {
 //   //   console.log(`l`);
 //   //   img1.style.transform = "translateX(-500px)";
 // });
-
+let num = 0;
 btnR.addEventListener("click", () => {
-  img1.classList.add("active") + 500;
-  console.log(33);
-  console.log(img1);
+  num++;
+  imgEl[num].style.transform = `translateX(-${num * 500}px)`;
+  console.log(num);
 });
+btnL.addEventListener("click", () => {
+  num--;
+  imgEl[num].style.transform = `translateX(${num * 500}px)`;
+  // console.log(num);
+});
+
+// function updateImg() {
+//   if (num > imgEl.length) {
+//     num = 0;
+//   }
+//   imgEl[num].style.transform = `translateX(-${num * 500}px)`;
+// }
